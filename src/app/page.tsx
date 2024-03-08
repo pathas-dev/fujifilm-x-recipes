cee92c180389c09f7fcec5a2a075be5a9e473425
pathas <pathas@naver.com>
2024-03-09 02:46:19 +0900

    Style(favicon) 이미지 변경, Feature(page) filter 데이터도 서버에서 같이 반환하도록 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,52 @@ import Card from '@/components/card/Card';
 import { Recipe } from '@/types/api';
 import { getAllDocuments } from './api/mongodb';
 
-const getRecipes = async (): Promise<any[]> => {
+const getRecipesWithFilters = async (): Promise<{
+  recipes: Recipe[];
+  filters: {
+    cameras: string[];
+    bases: string[];
+    sensors: string[];
+  };
+}> => {
   try {
     const data = await getAllDocuments('recipes');
-    return JSON.parse(JSON.stringify(data)) as Recipe[];
+    const recipes = JSON.parse(JSON.stringify(data)) as Recipe[];
+    const filters = recipes.reduce<{
+      cameras: string[];
+      bases: string[];
+      sensors: string[];
+    }>(
+      (acc, cur) => {
+        if (cur.camera && acc.cameras.indexOf(cur.camera) < 0)
+          acc.cameras.push(cur.camera);
+        if (cur.base && acc.bases.indexOf(cur.base) < 0)
+          acc.bases.push(cur.base);
+        if (cur.sensor && acc.sensors.indexOf(cur.sensor) < 0)
+          acc.sensors.push(cur.sensor);
+
+        return acc;
+      },
+      {
+        cameras: [],
+        bases: [],
+        sensors: [],
+      }
+    );
+
+    Object.values(filters).forEach((filter) => filter.sort());
+
+    return {
+      recipes,
+      filters,
+    };
   } catch (error) {
     throw new Error('Recipes data request failed');
   }
 };
 
 export default async function Home() {
-  const recipes = await getRecipes();
+  const { recipes, filters } = await getRecipesWithFilters();
 
   return (
     <main className="w-full h-fit p-2 flex flex-col gap-2">
