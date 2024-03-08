1248907178b05342db1b517170375d338d76b553
pathas <pathas@naver.com>
2024-03-08 14:47:05 +0900

    Style(ThemeSwitch) 스위치 svg 색상 변경

--- src/components/common/ThemeSwitch.tsx
+++ src/components/common/ThemeSwitch.tsx
@@ -65,7 +65,7 @@ const ThemeSwitch = () => {
 
       {/* moon icon */}
       <svg
-        className="swap-on fill-current w-10 h-10"
+        className="swap-on fill-sky-300 w-10 h-10"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
       >
@@ -74,7 +74,7 @@ const ThemeSwitch = () => {
 
       {/* sun icon */}
       <svg
-        className="swap-off fill-current w-10 h-10"
+        className="swap-off fill-amber-300 w-10 h-10"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
       >
