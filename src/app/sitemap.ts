74ff255b64a8327b51cda8e4766fb0c0613a117f
pathas <pathas@logiall.com>
2024-03-26 16:42:27 +0900

    SEO() sitemap, robots.txt 추가

---
+++ src/app/sitemap.ts
@@ -0,0 +1,23 @@
+import { locales } from '@/navigation';
+import { MetadataRoute } from 'next';
+
+const siteUrl = process.env.SITE_URL;
+
+const pages = ['', '/recipes', '/origins', '/bookmarks', '/cameras'];
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: `${siteUrl}`,
+      lastModified: new Date(),
+    },
+    ...locales.reduce<MetadataRoute.Sitemap>((acc, locale) => {
+      return acc.concat(
+        pages.map((page) => ({
+          url: `${siteUrl}/${locale}${page}`,
+          lastModified: new Date(),
+        }))
+      );
+    }, []),
+  ];
+}
