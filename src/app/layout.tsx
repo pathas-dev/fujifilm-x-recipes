039835fb4dff4302f59a91a489a8e30a90da15fb
pathas <pathas@logiall.com>
2024-04-04 16:00:04 +0900

    SEO() / 라우트 layout 에도 metadata 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,36 @@
+import { Metadata } from 'next';
 import './globals.css';
 import './rc-slider.css';
+import { SITE_URL } from './sitemap';
+
+export async function generateMetadata() {
+  // 도메인 만료 시 아래 vercel 링크로 변경
+
+  const metadata: Metadata = {
+    manifest: `${SITE_URL}/manifest.json`,
+    authors: [{ name: 'pathas', url: 'https://pathas.tistory.com/' }],
+    keywords: [
+      '후지필름',
+      '레시피',
+      '후지필름 레시피',
+      '필름 시뮬레이션',
+      '필름 레시피',
+      '카메라',
+      'fujifilm',
+      'recipes',
+      'fujifilm recipes',
+      'film simulation',
+      'film recipes',
+      'camera',
+    ],
+    icons: {
+      icon: `${SITE_URL}/favicon.ico`,
+      shortcut: `${SITE_URL}/favicon.ico`,
+    },
+    robots: { follow: true, index: true },
+  };
+  return metadata;
+}
 
 // Since we have a root `not-found.tsx` page, a layout file
 // is required, even if it's just passing children through.
