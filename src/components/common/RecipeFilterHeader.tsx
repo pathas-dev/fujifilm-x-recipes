265c55d51261d73a711cd88a402a898eef6eb5d7
pathas1126 <pathas@naver.com>
2025-07-19 03:16:47 +0900

    style: daisy v5 변경 사항 적용

--- src/components/common/RecipeFilterHeader.tsx
+++ src/components/common/RecipeFilterHeader.tsx
@@ -1,7 +1,7 @@
-import { HTMLInputTypeAttribute } from 'react';
-import _some from 'lodash/some';
-import { SvgFilmMicro } from '../icon/svgs';
-import { motion } from 'framer-motion';
+import { HTMLInputTypeAttribute, useState } from "react";
+import _some from "lodash/some";
+import { SvgFilmMicro } from "../icon/svgs";
+import { motion } from "framer-motion";
 
 export interface IRecipeFilterHeader {
   bwOnly: boolean;
@@ -22,8 +22,8 @@ const RecipeFilterHeader = ({
     <motion.header
       className="w-full h-14 shadow-md flex items-center p-2 bg-base-100 z-50 relative"
       transition={{ duration: 0.4 }}
-      initial={{ opacity: 0.3, translateY: '-150%' }}
-      animate={{ opacity: 1, translateY: '0%' }}
+      initial={{ opacity: 0.3, translateY: "-150%" }}
+      animate={{ opacity: 1, translateY: "0%" }}
     >
       {filters.map((dropboxProps, index) => (
         <Dropbox {...dropboxProps} key={index} />
@@ -73,27 +73,38 @@ export const Dropbox = ({
   onClickMenu,
   children,
   dropdownEnd,
-  type = 'checkbox',
+  type = "checkbox",
 }: IDropboxProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   let inputClassName =
-    type === 'checkbox'
-      ? 'checkbox checkbox-primary checkbox-xs'
-      : 'dropbox dropbox-primary dropbox-xs';
+    type === "checkbox"
+      ? "checkbox checkbox-primary checkbox-xs"
+      : "radio radio-primary radio-xs";
 
   return (
-    <details className={`dropdown ${dropdownEnd ? 'dropdown-end' : ''}`.trim()}>
-      <summary tabIndex={0} role="button" className="btn btn-sm m-1">
+    <div
+      className={`dropdown ${dropdownEnd ? "dropdown-end" : ""} ${
+        isOpen ? "dropdown-open" : ""
+      }`.trim()}
+    >
+      <div
+        tabIndex={0}
+        role="button"
+        className="btn btn-sm m-1"
+        onClick={() => setIsOpen(!isOpen)}
+      >
         {children}
-      </summary>
+      </div>
       <ul
         tabIndex={0}
-        className="dropdown-content menu p-2 shadow bg-base-100 rounded-box"
+        className="dropdown-content menu bg-base-100 rounded-box z-[1]  p-2 shadow"
       >
-        <div className="overflow-y-scroll max-h-64 w-max scroll-smooth">
+        <div className="overflow-y-auto max-h-64 w-full">
           {items.map((item) => (
             <li key={item.label}>
-              <label className="label cursor-pointer ">
-                <a className="label-text flex grow">{item.label}</a>
+              <label className="label cursor-pointer justify-between">
+                <span className="label-text">{item.label}</span>
                 <input
                   type={type}
                   checked={!!_some(selectedItems, item)}
@@ -107,7 +118,7 @@ export const Dropbox = ({
           ))}
         </div>
       </ul>
-    </details>
+    </div>
   );
 };
 
