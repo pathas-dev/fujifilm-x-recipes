282a6714e776c1fbeb904bf5b5a27e59678ccbe8
pathas1126 <pathas@naver.com>
2025-07-20 22:57:45 +0900

    feat: /chatbot [POST] 스트리밍 기능 추가, 프롬프트 및 리트리버 옵션 수정, 리팩토링

---
+++ src/app/api/chatbot/retrieval.ts
@@ -0,0 +1,51 @@
+import { PineconeEmbeddings, PineconeStore } from "@langchain/pinecone";
+import { Pinecone as PineconeClient } from "@pinecone-database/pinecone";
+import { BM25Retriever } from "@langchain/community/retrievers/bm25";
+import { EnsembleRetriever } from "langchain/retrievers/ensemble";
+
+export const createPineconeClient = () => {
+  return new PineconeClient({
+    apiKey: process.env.PINECONE_API_KEY!,
+  });
+};
+
+export const createEmbeddings = () => {
+  return new PineconeEmbeddings({
+    apiKey: process.env.PINECONE_API_KEY,
+    model: "multilingual-e5-large",
+  });
+};
+
+export const createVectorStore = async (
+  embeddings: PineconeEmbeddings,
+  pineconeIndex: any
+) => {
+  return await PineconeStore.fromExistingIndex(embeddings, {
+    pineconeIndex,
+    maxConcurrency: 50,
+  });
+};
+
+export const retrieve = async (query: string) => {
+  const pinecone = createPineconeClient();
+  const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX!);
+  const embeddings = createEmbeddings();
+  const vectorStore = await createVectorStore(embeddings, pineconeIndex);
+
+  const pineconeRetriever = vectorStore.asRetriever({
+    k: 3,
+    searchType: "mmr",
+    searchKwargs: { fetchK: 10 },
+  });
+
+  const documents = await pineconeRetriever.invoke(query);
+
+  const bm25Retriever = BM25Retriever.fromDocuments(documents, {
+    k: 3,
+  });
+
+  return new EnsembleRetriever({
+    retrievers: [pineconeRetriever, bm25Retriever],
+    weights: [0.8, 0.2],
+  }).invoke(query);
+};
